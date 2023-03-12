a087553f03984c3c11dbaf0611c4b46011a8c606
DiegoESMartinez <diegosalinasm.gi@gmail.com>
2023-03-13 00:06:26 +0100

    Update api_personajes.sql

--- Base de datos/api_personajes.sql
+++ Base de datos/api_personajes.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-03-2023 a las 02:53:42
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 7.4.30
+-- Tiempo de generación: 12-03-2023 a las 23:25:23
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,14 @@ CREATE TABLE `especies` (
   `ID` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `descripcion` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especies`
+--
+
+INSERT INTO `especies` (`ID`, `nombre`, `descripcion`) VALUES
+(2, 'criptoniano', 'cositas');
 
 -- --------------------------------------------------------
 
@@ -43,7 +50,14 @@ CREATE TABLE `generos` (
   `ID` int(11) NOT NULL,
   `Nombre` varchar(30) NOT NULL,
   `Descripcion` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`ID`, `Nombre`, `Descripcion`) VALUES
+(2, 'Mujer', 'Le palé');
 
 -- --------------------------------------------------------
 
@@ -60,7 +74,20 @@ CREATE TABLE `localizaciones` (
   `Dimension` varchar(40) NOT NULL,
   `Poblacion` int(11) NOT NULL,
   `Moneda` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `localizaciones`
+--
+
+INSERT INTO `localizaciones` (`ID`, `Coordenadas`, `Ciudad`, `Pais`, `Descripcion`, `Dimension`, `Poblacion`, `Moneda`) VALUES
+(1, '3242424,-523452', 'Cartagena', 'Espaliñita', 'Pais desertico e irreconocible', 'patata33', 434234, 'Bratislava'),
+(5, '3242424,-523452', 'Aerzrte', 'Deutasrt', 'Pais desertico e irreconocible', 'patata33', 2147483647, 'Bratislava'),
+(6, '3242424,-523452', 'Aerzrte', 'Deutasrt', 'Pais desertico e irreconocible', 'patata33', 2147483647, 'Bratislava'),
+(7, '3242424,-523452', 'Aerzrte', 'Deutasrt', 'Pais desertico e irreconocible', 'Patatita', 43, 'Eurito'),
+(8, '3242424,-523452', 'Aerzrte', 'Deutasrt', 'Pais desertico e irreconocible', 'Patatita', 43, 'Eurito'),
+(9, '3242424,-523452', 'Aerzrte', 'Deutasrt', 'Pais desertico e irreconocible', 'Patatita', 43, 'Eurito'),
+(10, '3242424,-523452', 'Aerzrte', 'Deutasrt', 'Pais desertico e irreconocible', 'ss', 43, 'Eurito');
 
 -- --------------------------------------------------------
 
@@ -82,7 +109,17 @@ CREATE TABLE `personajes` (
   `Nacimiento` int(11) NOT NULL,
   `Localizacion` int(11) NOT NULL,
   `Aparicion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `personajes`
+--
+
+INSERT INTO `personajes` (`ID`, `Nombre`, `Apellidos`, `Edad`, `Descripcion`, `Padre`, `Madre`, `Especie`, `Genero`, `Imagen`, `Nacimiento`, `Localizacion`, `Aparicion`) VALUES
+(8, 'Victores', 'oSSS', 44, 'accc', -1, -1, 2, 2, 'https://this-person-does-not-exist.com/gen/avatar-113fc90b9d2cb9e5f98f1e132a1dfec4.jpg', 1, 1, 1),
+(14, 'Victores 2', 'oSSS', 443432, 'Personaje Random', -1, -1, 2, 2, 'https://this-person-does-not-exist.com/gen/avatar-113fc90b9d2cb9e5f98f1e132a1dfec4.jpg', 1, 1, 1),
+(15, 'Victores 2', 'oSSSda', 4434324, 'Personaje Random', -1, -1, 2, 2, 'https://this-person-does-not-exist.com/gen/avatar-113fc90b9d2cb9e5f98f1e132a1dfec4.jpg', 1, 8, 1),
+(17, 'Victores 2', 'oSSSda', 4434324, 'Personaje Random', -1, 15, 2, 2, 'https://this-person-does-not-exist.com/gen/avatar-113fc90b9d2cb9e5f98f1e132a1dfec4.jpg', 1, 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -127,25 +164,25 @@ ALTER TABLE `personajes`
 -- AUTO_INCREMENT de la tabla `especies`
 --
 ALTER TABLE `especies`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `generos`
 --
 ALTER TABLE `generos`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `localizaciones`
 --
 ALTER TABLE `localizaciones`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `personajes`
 --
 ALTER TABLE `personajes`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Restricciones para tablas volcadas
@@ -155,11 +192,11 @@ ALTER TABLE `personajes`
 -- Filtros para la tabla `personajes`
 --
 ALTER TABLE `personajes`
-  ADD CONSTRAINT `personajes_ibfk_1` FOREIGN KEY (`Nacimiento`) REFERENCES `localizaciones` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `personajes_ibfk_2` FOREIGN KEY (`Localizacion`) REFERENCES `localizaciones` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `personajes_ibfk_3` FOREIGN KEY (`Aparicion`) REFERENCES `localizaciones` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `personajes_ibfk_4` FOREIGN KEY (`Genero`) REFERENCES `generos` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `personajes_ibfk_5` FOREIGN KEY (`Especie`) REFERENCES `especies` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `personajes_ibfk_1` FOREIGN KEY (`Especie`) REFERENCES `especies` (`ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `personajes_ibfk_2` FOREIGN KEY (`Nacimiento`) REFERENCES `localizaciones` (`ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `personajes_ibfk_3` FOREIGN KEY (`Localizacion`) REFERENCES `localizaciones` (`ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `personajes_ibfk_4` FOREIGN KEY (`Aparicion`) REFERENCES `localizaciones` (`ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `personajes_ibfk_5` FOREIGN KEY (`Genero`) REFERENCES `generos` (`ID`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
